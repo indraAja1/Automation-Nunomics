deb53d238539b16f97f37a754dcfbbd2715ae66c
Diados <indradimas234@gmail.com>
2024-08-23 16:37:14 +0700

    Update Login

---
+++ Login Case/login_email.py
@@ -0,0 +1,59 @@
+from open_app import open_app
+import unittest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Variable ID
+# Di ambil dari APPIUM INSPECTOR
+field_email = 'com.nunomics.app.debug:id/etUsernameEmail'
+field_pass = 'com.nunomics.app.debug:id/etPassword'
+btn_login_id = 'com.nunomics.app.debug:id/btnApply'
+btn_notif_id = 'com.android.permissioncontroller:id/permission_allow_button'
+
+# Variable Input
+input_email = "usertesting1satu@gmail.com"
+input_pass = "Testing1"
+
+class OpenNunomics(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = open_app()  # Pastikan open_app() mengembalikan driver
+        if not self.driver:
+            raise Exception("Driver tidak berhasil diinisialisasi dari open_app()")
+        
+    def test_loginsucces(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((AppiumBy.ID, field_email))
+            )
+            input_field = WebDriverWait(self.driver, 5).until(
+                EC.visibility_of_element_located((AppiumBy.ID, field_email))
+            )
+            input_field.clear()
+            input_field.send_keys(input_email)
+
+            input_field_password = WebDriverWait(self.driver, 5).until(
+                EC.visibility_of_element_located((AppiumBy.ID, field_pass))
+            )
+            input_field_password.clear()
+            input_field_password.send_keys(input_pass)
+
+            btn_login = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((AppiumBy.ID, btn_login_id))
+            )
+            btn_login.click()
+            
+            WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((AppiumBy.ID, btn_notif_id))
+            )
+            btn_notif = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((AppiumBy.ID, btn_notif_id))
+            )
+            btn_notif.click()
+            print("Sukses Login")
+
+        except Exception as e:
+            print(f"Terjadi kesalahan saat login: {e}")
+
+if __name__ == "__main__":
+    unittest.main()
